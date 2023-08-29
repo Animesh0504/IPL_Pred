b13c516333bf9dd58677cd6788b89d939c8dd017
Animesh Dutt Mishra <109816950+Animesh0504@users.noreply.github.com>
2023-08-29 21:06:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title='IPL_Score_Predictor',layout="centered")
 
 #Get the ML model 
 
-filename='tree_ml_model.pkl'
+filename='ml_model_ipl.pkl'
 model = pickle.load(open(filename,'rb'))
 
 #Title of the page with CSS
